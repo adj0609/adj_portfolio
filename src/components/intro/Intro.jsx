3dfd22ddb9df9c954d564c167764b5d216428cea
Andrea Jared <adj0803@gmail.com>
2023-08-07 10:00:05 -0400

    mapping over portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,29 @@
 import './intro.scss'
+import Down from '@mui/icons-material/KeyboardDoubleArrowDown';
 
-export default function intro() {
+
+
+
+export default function Intro() {
   return (
-    <div className='intro' id='intro'></div>
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className="imgContainer">
+          <img src='./assets/Andrea Jared Copy.png' alt='Andrea Jared' />
+        </div>
+      </div>
+        <div className='right'>
+          <div className='wrapper'>
+            <h2>Hi There, I'm</h2>
+            <h1>Andrea Jared</h1>
+            <h3>Junior <span>Full-Stack</span> Developer</h3>
+        </div>
+        <a href='#portfolio'>
+        <Down className='icon'/>
+        
+          </a>
+      </div>
+    </div>
   )
 }
+
